fddc1c472a5e063a4ad29d7efa7bcb453050f7ad
Grace-Shao <graceshao203@gmail.com>
2022-11-24 17:43:13 -0500

    player can now pick up items and drop them

---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    // should these all be public?
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, itemContainer;
+
+    public float pickupRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        // Check if player is in range and "E" is pressed
+        Vector3 distanceToPlayer = player.position - transform.position;
+        Debug.Log("dist from player" + distanceToPlayer);
+        if (!equipped && distanceToPlayer.magnitude <= pickupRange && Input.GetKeyDown(KeyCode.E) && !slotFull)
+        {
+            Debug.Log("Picked up");
+            Pickup();
+        }
+
+        // Drop if equipped and "Q" is pressed
+        if (equipped && Input.GetKeyDown(KeyCode.Q))
+        {
+            Drop();
+        }
+    }
+
+    private void Pickup()
+    {
+        equipped = slotFull = true;
+
+        // make weapon a chold of the camera and move it to the default position
+        transform.SetParent(itemContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        // make rigidBody kinematic (so item doesn't move anymore) and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+    }
+
+    // why does boxCollider need to be a trigger
+    private void Drop()
+    {
+        equipped = slotFull = false;
+
+        // set parent to null (parent used to be itemContainer)
+        transform.SetParent(null);
+
+        // make rigidBody not kinematic and BoxCollider normal
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+    }
+}
