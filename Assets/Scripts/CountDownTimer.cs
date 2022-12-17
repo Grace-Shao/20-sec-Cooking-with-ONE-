d918adf2a93daf444da5d72c62fc2066938498ae
Grace-Shao <graceshao203@gmail.com>
2022-12-17 14:15:48 -0500

    countdown timer done, vs having red errors everywhere maybe fixed?

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CountDownTimer : MonoBehaviour
 {
     float currentTime = 0f;
     float startingTime = 20f;
 
-    private void Start()
+    // same as private but we can see it in field
+    [SerializeField] TextMeshProUGUI countdownText;
+
+    private void Awake()
     {
         currentTime = startingTime;
     }
 
     private void Update()
     {
+        if (currentTime >= 0)
+        {
+            countDown();
+        }
+    }
+
+    private void countDown()
+    {
         // time.deltaTime The interval in seconds from the last frame to the current one 
         currentTime -= 1 * Time.deltaTime;
-
+        countdownText.text = currentTime.ToString("0");
     }
 }
