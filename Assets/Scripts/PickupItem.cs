cdbe1ea502ca417efcb7d1fdc914fcb136762954
Grace-Shao <graceshao203@gmail.com>
2022-12-23 17:23:29 -0500

    only be able to pick up one thing at a time, player is taller now

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -32,17 +32,26 @@ public class PickupItem : MonoBehaviour
 
     private void Pickup()
     {
-        equipped = true;
+        // if itemcontainer alr has an object, "drop the item"
+        if (itemContainer.childCount > 0)
+        {
+            return;
+        } 
+        else
+        {
+            equipped = true;
 
-        // make weapon a chold of the camera and move it to the default position
-        transform.SetParent(itemContainer);
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.Euler(Vector3.zero);
+            // make weapon a chold of the camera and move it to the default position
+            transform.SetParent(itemContainer);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.Euler(Vector3.zero);
 
-        // make rigidBody kinematic (so item doesn't move anymore) and BoxCollider a trigger
-        //rb.isKinematic = true;
-        //rb.detectCollisions = false;
-        coll.isTrigger = true;
+            // make rigidBody kinematic (so item doesn't move anymore) and BoxCollider a trigger
+            //rb.isKinematic = true;
+            //rb.detectCollisions = false;
+            coll.isTrigger = true;
+        }
+        
     }
 
     // why does boxCollider need to be a trigger
