6c89c635a1a575d23eb9c1baffa5af3e223b5671
Grace-Shao <graceshao203@gmail.com>
2022-12-23 16:39:50 -0500

    cheese is pickable

--- Assets/Scripts/ChopAction.cs
+++ Assets/Scripts/ChopAction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChopAction : MonoBehaviour
 {
-    public GameObject tomatoSlices; // may be bad programming
+    [SerializeField] private GameObject tomatoSlices; // may be bad programming
     void OnEnable()
     {
         EventManager.onChop += Chopping;
--- Assets/Scripts/CombineIngredientsAction.cs
+++ Assets/Scripts/CombineIngredientsAction.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// similar to the chop action but uses serializable field (is this good better programming practice
+// or chop actions?
+
 public class CombineIngredientsAction : MonoBehaviour
 {
+    [SerializeField] private Transform plate;
     void OnEnable()
     {
         EventManager.onCombineIngredients += Combine;
@@ -16,6 +20,19 @@ public class CombineIngredientsAction : MonoBehaviour
 
     void Combine()
     {
+        Vector3 playerPosition = GetComponent<Transform>().position;
+        float playerToPlateRange = 2f;
+        float distance = (playerPosition - plate.position).magnitude;
+
+        // if close to plate
+        if (distance < playerToPlateRange)
+        {
+
+        }
+        
+        // if child one has tomato or cheese, put tomato or cheese on plate but child 2 is empty
+        //      if child 1 has the other ingredient, combine to make pizza
+        // if both child one and child two have things in them, being near plate will make pizza
 
     }
 }
