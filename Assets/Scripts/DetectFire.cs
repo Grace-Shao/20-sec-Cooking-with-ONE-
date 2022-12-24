ad50e6346a878838c27ba8ad6f0745b89cace8d2
Grace-Shao <graceshao203@gmail.com>
2022-12-23 21:23:06 -0500

    working on pizza can detect fire script

---
+++ Assets/Scripts/DetectFire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("particle collision");
+    }
+}
